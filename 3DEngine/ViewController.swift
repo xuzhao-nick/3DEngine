c9934c502e70af547089bd78597a8530e4681f9c
Xu Zhao <brainstudiosg@gmail.com>
2014-10-18 23:54:58 +0800

    Initial Commit

---
+++ 3DEngine/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  3DEngine
+//
+//  Created by Xu Zhao on 18/10/14.
+//  Copyright (c) 2014 Xu Zhao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
