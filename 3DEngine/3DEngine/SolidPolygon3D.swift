a7b83c30fd8cbf4b64590e158ed7e4be1d858f2b
Xu Zhao <brainstudiosg@gmail.com>
2014-10-19 23:05:08 +0800

    Add SolidPolygon3D class

---
+++ 3DEngine/3DEngine/SolidPolygon3D.swift
@@ -0,0 +1,18 @@
+//
+//  SolidPolygon3D.swift
+//  3DEngine
+//
+//  Created by Xu Zhao on 19/10/14.
+//  Copyright (c) 2014 Xu Zhao. All rights reserved.
+//
+
+import UIKit
+
+/**
+The SolidPolygon3D class is the Child class of Polygon3D and have color attribute
+*/
+class SolidPolygon3D:Polygon3D {
+    
+    var color:UIColor = UIColor.greenColor()
+    
+}
---
+++ 3DEngine/DrawView.swift
@@ -0,0 +1,9 @@
+//
+//  DrawView.swift
+//  3DEngine
+//
+//  Created by Xu Zhao on 19/10/14.
+//  Copyright (c) 2014 Xu Zhao. All rights reserved.
+//
+
+import Foundation
