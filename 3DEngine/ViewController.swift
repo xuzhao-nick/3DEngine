3070d1afffb447c1671ee02367495c28b4f3ff07
Xu Zhao <brainstudiosg@gmail.com>
2014-10-19 23:34:44 +0800

    add zoom in/zoom out and rotate effect

--- 3DEngine/ViewController.swift
+++ 3DEngine/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var drawView: DrawView!
+    
+    let ratio:Float = 20
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        var timer = NSTimer.scheduledTimerWithTimeInterval(0.02, target: self, selector: Selector("update"), userInfo: nil, repeats: true)
+        
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,13 +27,23 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func update() {
+        // Something cool
+        // rotate around the y-axis
+        drawView.treeTransform.rotateAngleY(0.002 * ratio)
+        drawView.setNeedsDisplay()
+    }
     
     @IBAction func zoomInButtonPressed(sender: UIButton) {
         
+        drawView.treeTransform.location.z += 0.5 * ratio;
+        drawView.setNeedsDisplay()
     }
 
     @IBAction func zoomOutButtonPressed(sender: UIButton) {
         
+        drawView.treeTransform.location.z -= 0.5 * ratio;
+        drawView.setNeedsDisplay()
     }
     
 
